e5c7c4b4d40e7e8be30c11d500156a689982392c
Λsh Dev <69819379+ash-dvlpr@users.noreply.github.com>
2024-02-05 20:02:02 +0100

    Disabled - Custom Attribute

---
+++ Runtime/Attributes/Disabled.cs
@@ -0,0 +1,11 @@
+using System;
+using UnityEngine;
+
+
+namespace CinderUtils.Attributes { 
+    /// <summary>
+    /// Marks a field to be disabled on the the Editor.
+    /// </summary>
+    [AttributeUsage(AttributeTargets.Field)]
+    public class DisabledAttribute : PropertyAttribute { }
+}
