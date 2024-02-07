532dd263bf55f197b1dd0029e9c6e889617ce74f
Ash Dev <69819379+ash-dvlpr@users.noreply.github.com>
2024-02-07 17:03:54 +0100

    Aded v1 of the AssemblyUtils.

---
+++ Runtime/Reflection/AssemblyUtils.cs
@@ -0,0 +1,47 @@
+using System;
+using System.Reflection;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using CinderUtils.Extensions;
+
+
+namespace CinderUtils.Reflection {
+
+    public static partial class AssemblyUtils {
+        public static IReadOnlyDictionary<AssemblyType, Assembly> AsseblyCache {
+            get => asseblyCache;
+        }
+
+        // Predefined Assemblies: https://docs.unity3d.com/Manual/ScriptCompileOrderFolders.html 
+        public enum AssemblyType : byte {
+            Unknown = 0,
+            Assembly_CSharp_FirstPass           = 1, // Assembly-CSharp-firstpass
+            Assembly_CSharpEditor_FirstPass     = 2, // Assembly-CSharp-Editor-firstpass
+            Assembly_CSharp                     = 3, // Assembly-CSharp
+            Assembly_CSharpEditor               = 4, // Assembly-CSharp-Editor
+        }
+
+        // Returns all the subtypes of some Type T inside the target AssemblyTypes.
+        // By default will only look in the Assembly_CSharp, and Assembly_CSharpEditor assemblies.
+        public static List<Type> GetSubtypesOf<T>(ICollection<AssemblyType> targetAssemblies = null) {
+            if (targetAssemblies.NullOrEmpty()) {
+                targetAssemblies = new List<AssemblyType>() {
+                    AssemblyType.Assembly_CSharp,
+                    AssemblyType.Assembly_CSharpEditor,
+                };
+            }
+
+            List<Type> subtypes = new();
+            foreach (var target in targetAssemblies) {
+                if (AsseblyCache.ContainsKey(target)) { 
+                    subtypes.AddRange(GetSubtypesInAssemblyOf<T>(AsseblyCache[target]));
+                }
+            }
+
+            return subtypes;
+        }
+    }
+
+}
---
+++ Runtime/Reflection/Internal.cs
@@ -0,0 +1,70 @@
+using System;
+using System.Reflection;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using CinderUtils.Extensions;
+
+
+namespace CinderUtils.Reflection {
+
+    public static partial class AssemblyUtils {
+        internal static Dictionary<AssemblyType, Assembly> asseblyCache;
+
+        [RuntimeInitializeOnLoadMethod(RuntimeInitializeLoadType.SubsystemRegistration)]
+        internal static void Initialize() {
+            asseblyCache = GetAssemblies();
+            Debug.Log("CinderUtils.Reflection: AssemblyUtils initialized.");
+        }
+
+        internal static AssemblyType? GetAssemblyType(string assemblyName) {
+            return assemblyName switch {
+                "Assembly-CSharp" => AssemblyType.Assembly_CSharp,
+                "Assembly-CSharp-Editor" => AssemblyType.Assembly_CSharpEditor,
+                "Assembly-CSharp-firstpass" => AssemblyType.Assembly_CSharp_FirstPass,
+                "Assembly-CSharp-Editor-firstpass" => AssemblyType.Assembly_CSharpEditor_FirstPass,
+                _ => null
+            };
+        }
+
+        internal static bool TryGetAssemblyType(string assemblyName, out AssemblyType? type) {
+            type = GetAssemblyType(assemblyName);
+            return type != null && type != AssemblyType.Unknown;
+        }
+
+        // Returns a collection of Types per AssemblyType.
+        internal static Dictionary<AssemblyType, Assembly> GetAssemblies() {
+            Assembly[] assemblies = AppDomain.CurrentDomain.GetAssemblies();
+            Dictionary<AssemblyType, Assembly> assemblyCache = new();
+
+            // Extract all the types from the defined assemblies (AssemblyType)
+            foreach (Assembly asm in assemblies) {
+                if (TryGetAssemblyType(asm.GetName().Name, out var asmType)) {
+                    assemblyCache.Add((AssemblyType) asmType, asm);
+                }
+            }
+
+            return assemblyCache;
+        }
+
+        internal static List<Type> GetSubtypesInAssemblyOf<T>(Assembly assembly) {
+            if (assembly == null) return null;
+
+            var parentType = typeof(T);
+            List<Type> subtypes = new();
+
+            foreach (Type type in assembly.GetTypes()) {
+                // Could variable for type "parentType" get assigned a value of type "type"?
+                // If so, the type is a subtype
+                if (parentType.IsAssignableFrom(type)) {
+                    Debug.Log($"CinderUtils: AssemblyUtils: Found Subtype of '{parentType}': '{type.Name}' in Assembly '{assembly.GetName().Name}'.");
+                    subtypes.Add(type);
+                }
+            }
+
+            return subtypes;
+        }
+    }
+
+}
